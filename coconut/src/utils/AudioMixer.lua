923227b8ec43e9ec938a304aeb0e50e2a9bf701f
KiwiSky <kiwisky94@gmail.com>
2025-07-02 11:04:06 -0300

    heady

--- coconut/src/utils/AudioMixer.lua
+++ coconut/src/utils/AudioMixer.lua
@@ -1,7 +1,26 @@
-local AudioChannel = class:extend("AudioChannel")
+local vec2 = import 'Math.Vec2'
+local assets = import 'utils.AssetPool'
 
+local AudioGroup = class:extend("AudioChannel")
+
+---@class Coconut.utils.AudioMixer.AudioChannel
+---@field volume number
+---@field pitch number
+---@field position Vec2
+---@field private assetname string
+---@field private source love.audio.Source
 function AudioChannel:__construct()
+    self.volume = 0.75
+    self.pitch = 1
+    self.position = vec2.ZERO()
+    self.assetName = "" 
+    self.source = nil
+    self.loadType = assets.AudioLoadType.STREAM
+end
 
+function AudioChannel:useAsset(name)
+    self.assetName = name
+    self.source = assets.get(assets.AssetType.AUDIO, self.assetName, {})
 end
 
 ---@class Coconut.utils.AudioMixer
@@ -17,6 +36,11 @@ local AudioMixer = {
 }
 
 function AudioMixer.newChannel(name)
+    
+end
+
+---@private
+function AudioMixer.update()
     
 end
 
